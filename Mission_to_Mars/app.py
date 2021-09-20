d619b2370481753fbf0176e9549216a4969869db
unknown <vmatthew360@gmail.com>
2021-09-19 20:05:20 -0500

    Updated app.py as well as index home page

--- Mission_to_Mars/app.py
+++ Mission_to_Mars/app.py
@@ -6,7 +6,7 @@ import scrape_mars
 app = Flask(__name__)
 
 # Use PyMongo to establish Mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/weather_app")
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
 
 # Route to render index.html template using data from Mongo
@@ -14,10 +14,10 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/weather_app")
 def index():
 
     # Find one record of data from the mongo database
-    mars_dict = mongo.db.mars_dict.find_one()
+    mongo_data = mongo.db.collection.find_one()
 
     # Return template and data
-    return render_template("index.html", mars_dict=mars_dict)
+    return render_template("index.html", mars=mongo_data)
 
 
 # Route that will trigger the scrape function
--- Mission_to_Mars/scrape_mars.py
+++ Mission_to_Mars/scrape_mars.py
@@ -2,41 +2,89 @@ from splinter import Browser
 from bs4 import BeautifulSoup as bs
 import pandas as pd
 from webdriver_manager.chrome import ChromeDriverManager
-
+import requests
 
 def scrape_info():
+
     # Set up Splinter
     executable_path = {'executable_path': ChromeDriverManager().install()}
     browser = Browser('chrome', **executable_path, headless=False)
 
-    # Visit Mars site
+    # Visit URL
     url = "https://redplanetscience.com/"
     browser.visit(url)
+    soup = bs(browser.html, 'lxml')
 
-    # Scrape page into Soup
-    html = browser.html
-    soup = bs(html, "html.parser")
+    news_title = soup.find("div", class_ = "content_title").text
+    news_paragraph = soup.find("div", class_ = "article_teaser_body").text
 
-    # results = soup.find_all("div", class_="list_text")
-    # result = results[0]
+    # Visit Image url through splinter module
+    url_marsimage = "https://spaceimages-mars.com/"
+    browser.visit(url_marsimage)
 
-    status = True
-    while status:
-        try:
-            results = soup.find_all('div', class_='list_text')
-            result = results[0]
-            status = False
-        except:
-            pass
-    
-    news_title = result.find("div", class_="content_title").text().strip()
-    news_paragraph = result.find("div", class_="article_teaser_body").text().strip()
+    # HTML Object
+    img_html = browser.html
+    img_soup = bs(img_html, "html.parser")
 
+    # Find image url to the full size
+    featured_image = img_soup.find("img", class_ = "headerimage fade-in").text
+
+    # Display url of the full image
+    featured_image_url = f"https://spaceimages-mars.com/{featured_image}"
+
+    # Visit the galaxy Facts webpage and use Pandas to scrape the table
+    url_facts = "https://galaxyfacts-mars.com/"
+
+
+    # Use Pandas to scrape tabular data from a page
+    mars_facts = pd.read_html(url_facts)
+
+    mars_df = mars_facts[0]
+
+    mars_df.columns = ["Description", "Mars", "Earth"]
+    mars_df.set_index("Description", inplace=True)
+
+    html_facts = mars_df.to_html()
+
+    # clean up the table
+    html_facts.replace("\n", '')
+
+    # Save html code
+    mars_df.to_html("mars_facts_data.html")
+
+    html_facts = mars_df.to_html(classes = 'table table-striped')
+
+    # Visit the hemisphere site
+    url_hem = "https://marshemispheres.com/"
+    hem_response = requests.get(url_hem)
+    hem_soup = bs(hem_response.text, 'html.parser')
+
+    browser.visit(url_hem)
+
+    hem_img_urls = []
+
+    hem_links = browser.find_by_css('a.product-item img')
+
+    for link in range(len(hem_links)):
+            browser.find_by_css('a.product-item img')[link].click()
+            image = browser.links.find_by_text("Sample").first
+            
+            image_url = image['href']
+            pic_title = browser.find_by_css("h2.title").text
+            hem_img_urls.append({"image_url": image_url, "pic_title": pic_title})
+            browser.back()
+            
+
+
+    mars_data = {
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image_url": featured_image_url,
+        "mars_facts": html_facts,
+        "hem_img_urls": hem_img_urls}
     # Close the browser after scraping
+
     browser.quit()
 
-    mars_news_dict ={}
-    mars_news_dict['news_title'] = news_title
-    mars_news_dict['news_paragraph'] = news_paragraph
     # Return results
-    return (mars_news_dict)
+    return (mars_data)
